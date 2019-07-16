963a7ab572771d7f9f06a9be43c3a4b84c2540ab
hiennm <hiennm0406@gmail.com>
2019-07-16 22:40:33 +0700

    Add Battle Map

---
+++ ZombieVsPlant/Assets/Scrpits/Battle/MapController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapController : MonoBehaviour
+{
+    public List<List<Transform>> Map = new List<List<Transform>>();
+    // Start is called before the first frame update
+    void Start()
+    {       
+        int row = 0;
+        int col = 0;
+        foreach(Transform child in transform)
+        {
+            if(row == Map.Count)
+            {
+                Map.Add(new List<Transform>());
+            }
+            Debug.Log(row+" - "+col);
+            Map[row].Add(child);
+            col++;
+            if(col == 10)
+            {
+                col = 0;
+                row++;
+            }
+        }
+    }
+
+}
---
+++ ZombieVsPlant/Assets/Scrpits/Battle/PlaceController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceController : MonoBehaviour
+{
+    public GameObject target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray r = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(r.origin, r.direction);
+            if(hit.collider != null && hit.collider.tag == "ZombieStart")
+            {
+                Instantiate(target, hit.collider.transform.position, Quaternion.identity);
+            }
+        }
+    }
+}
